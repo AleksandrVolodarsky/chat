077c1148e59f1232601e154c1701fda219572e66
gcofficial <gurievcreative@gmail.com>
2018-05-05 00:58:44 +0300

    short info in sidebar

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -3,7 +3,7 @@
     <at-menu mode="vertical" active-name="1" @on-select="clickMenu">
       <div class="short-info">
         <div class="left">
-          <app-avatar size="40" :name="user.name" url=""></app-avatar>
+          <app-avatar :size="40" :name="user.name" url=""></app-avatar>
         </div>
         <div class="right">
           <b>{{ user.name }}</b>
